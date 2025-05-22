2a10b94988fb8ca8b5c707be9095ea28165db674
Sonu2707 <Saunvir.Singh07@gmail.com>
2025-05-23 00:34:26 +0530

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,25 @@
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+}
+async function startDownload() {
+    const url = document.getElementById("urlInput").value;
+    const quality = document.getElementById("quality").value;
+    const progressBar = document.getElementById("progressBar");
+    progressBar.style.display = "block";
+    progressBar.value = 20;
+
+    const res = await fetch("http://localhost:8000/download/", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({url: url, quality: quality})
+    });
+
+    progressBar.value = 80;
+    const blob = await res.blob();
+    const filename = res.headers.get("Content-Disposition").split("filename=")[1];
+    const a = document.createElement("a");
+    a.href = window.URL.createObjectURL(blob);
+    a.download = filename;
+    a.click();
+    progressBar.value = 100;
+}
